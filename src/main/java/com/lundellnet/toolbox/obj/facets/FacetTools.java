22d24c7aa8e881b2672b4846e45670077150ff8e
Nate Lundell <nate@lundellnet.com>
2017-04-24 12:33:11 -0500

    added FacetTools

---
+++ src/main/java/com/lundellnet/toolbox/obj/facets/FacetTools.java
@@ -0,0 +1,11 @@
+package com.lundellnet.toolbox.obj.facets;
+
+import java.util.stream.Stream;
+
+public class FacetTools {
+
+	public static <F extends Facet<?, ?>> Stream<F> streamMatrixSourceFacets(Class<?> matrixSourceClass) {
+		//TODO uhhhh do.
+		return null;
+	}
+}
